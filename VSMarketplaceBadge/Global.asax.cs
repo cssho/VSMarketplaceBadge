d6626866b51bd1b20eb7ffc46e988a4ebd7b1599
cssho <rizeupbass@gmail.com>
2020-01-20 14:10:24 +0900

    update

--- VSMarketplaceBadge/Global.asax.cs
+++ VSMarketplaceBadge/Global.asax.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Security;
-using System.Security.Cryptography.X509Certificates;
-using System.Web;
-using System.Web.Http;
-using System.Web.Routing;
+﻿using System.Web.Http;
 
 namespace VSMarketplaceBadge
 {
@@ -15,15 +7,6 @@ namespace VSMarketplaceBadge
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            ServicePointManager.ServerCertificateValidationCallback =
-                new RemoteCertificateValidationCallback(
-                    OnRemoteCertificateValidationCallback);
         }
-
-        private bool OnRemoteCertificateValidationCallback(
-            Object sender,
-            X509Certificate certificate,
-            X509Chain chain,
-            SslPolicyErrors sslPolicyErrors) => true;
     }
 }
